bab9bcf680d1ed8e81430b3ac468487d81d90a8d
WAFFLES\Hemanth Potluri <potlurih@gmail.com>
2016-09-03 22:11:49 -0400

    Added print statement to code

---
+++ PotluriHemanthH0/Hello.java
@@ -0,0 +1,17 @@
+/**
+   This program prints out Hello World
+   
+   ICSI213
+   
+   Hemanth Potluri 
+   
+   9/3/2016
+*/
+
+public class Hello 
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World");  //Prints out Hello World
+  }
+}
