5c0d04d20b5b43be383cb4dedc8dbc015b22ee34
WAFFLES\Hemanth Potluri <potlurih@gmail.com>
2016-09-17 10:37:29 -0400

    Add the food test to test class

--- PotluriHemanthH1/Test.java
+++ PotluriHemanthH1/Test.java
@@ -13,6 +13,14 @@ public class Test {
 
     public static void main(String[] args){
  
+        // Food object creator and tester 
+        Food testFood = new Food("Chocolate", 25, 500, 5);
+        testFood.examine();
+        System.out.println(testFood.getName());
+        System.out.println(testFood.getWeight());
+        System.out.println(testFood.getNutrition());
+        System.out.println(testFood.getQuantity());
+        System.out.println('\n');
     
     
     }
