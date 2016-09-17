4c4c4a1cbfa5ca3b30af509a2f8d0b376b704a72
WAFFLES\Hemanth Potluri <potlurih@gmail.com>
2016-09-17 11:46:59 -0400

    Revise names and add weapon test to test class

--- PotluriHemanthH1/Test.java
+++ PotluriHemanthH1/Test.java
@@ -13,6 +13,13 @@ public class Test {
 
     public static void main(String[] args){
  
+        // Item object creator and tester
+        Item testItem = new Item("Ring", 69);
+        testItem.examine();
+        System.out.println(testItem.getName());
+        System.out.println(testItem.getWeight());
+        System.out.println('\n');
+       
         // Food object creator and tester 
         Food testFood = new Food("Chocolate", 25, 500, 5);
         testFood.examine();
@@ -23,11 +30,22 @@ public class Test {
         System.out.println('\n');
     
         // Armor object creator and tester
-        Armor testArmor = new Armor(45, "Armor", 62);
+        Armor testArmor = new Armor(45, "Plate Armor", 62);
         testArmor.examine();
         System.out.println(testArmor.getName());
         System.out.println(testArmor.getWeight());
         System.out.println(testArmor.getDefense());
+        System.out.println('\n');
+        
+         // Create a Weapon object and test it
+        Weapon testWeapon = new Weapon("Rapier", 23, 54);
+        testWeapon.examine();
+        System.out.println(testWeapon.getName());
+        System.out.println(testWeapon.getWeight());
+
+        // Change damage and print the new value
+        testWeapon.setDamage(65);
+        System.out.println(testWeapon.getDamage());
         System.out.println('\n');
     
     }
