affb5af5c2742918e192daf845492ff55e68e236
WAFFLES\Hemanth Potluri <potlurih@gmail.com>
2016-09-17 18:56:14 -0400

    Add constructor to the item class

--- PotluriHemanthH1/Item.java
+++ PotluriHemanthH1/Item.java
@@ -9,7 +9,14 @@
  */
 public class Item {
   
+  private String name;
+  private int weight;
   
+    //Constructor
+    public Item(String name, int weight){
+        this.name = name;
+        this.weight = weight;
+    }
 
 
 }
