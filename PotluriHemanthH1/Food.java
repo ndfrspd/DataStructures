d675b8b109c5a158f1e5f6aa5d3b5e0d46fb10d0
WAFFLES\Hemanth Potluri <potlurih@gmail.com>
2016-09-17 22:18:00 -0400

    Add get and set method for nutrition, get and set method for quantity

--- PotluriHemanthH1/Food.java
+++ PotluriHemanthH1/Food.java
@@ -13,5 +13,30 @@ public class Food extends Item {
   private int nutrition;
   private int quantity;
 
+  //Constructor
+  public Food(String name, int weight, int nutrition, int quantity) {
+    super(name, weight);
+    this.nutrition = nutrition;
+    this.quantity = quantity;
+    }
   
+  // Get nutrition method
+  public int getNutrition() {
+    return nutrition;
+    }
+    
+  // Set Nutrition method
+  public void setNutrition(int nutrition) {
+    this.nutrition = nutrition;
+    }
+
+  // Get quantity method
+  public int getQuantity() {
+    return quantity;
+    }
+
+  // Set Quantity method
+  public void setQuantity(int quantity) {
+    this.quantity = quantity;
+    }
 }
